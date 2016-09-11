fb8fa4a9f09d82a92a3ec51b24a983dc91c16130
xiaowu <xiaowu@localhost.localdomain>
2016-09-11 21:53:47 +0800

    example about travel

---
+++ myexpress/index.js
@@ -0,0 +1,59 @@
+var express = require("express")
+
+var app = express();
+
+
+var handlebars = require('express3-handlebars')
+    .create({defaultLayout: 'main'});
+app.engine('handlebars', handlebars.engine);
+app.set('view engine', 'handlebars');
+
+app.set('port', process.env.PORT || 3000);
+
+
+app.use(express.static(__dirname + '/public'));
+
+
+
+app.get('/', function (req, res) {
+  res.render('home');
+});
+
+
+app.get('/about', function (req, res) {
+  var randomTest =
+      testArray[Math.floor(Math.random() * testArray.length)];
+  res.render('about', {testArray: randomTest});
+});
+
+
+
+
+
+app.use(function (req, res) {
+  res.status(404)
+  res.render('404');
+  
+});
+
+
+
+app.use(function (err, req, res, next) {
+  console.log(err.stack);
+  res.status(500)
+  res.render('500');
+});
+
+
+app.listen(app.get('port'), function () {
+  console.log('Express started on http://localhost:' + app.get('port') +
+              '; press Ctrl + C to terminate');
+});
+
+
+
+var testArray = [
+  "1",
+  "2",
+  "3"
+];
