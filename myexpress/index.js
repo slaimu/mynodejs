a41cab23af5bd10ff572eedbf329a1c40f5d0fd4
Xiaowu Zhang <xiaowu.zhangv@gmail.com>
2016-09-15 02:05:52 +0800

    simple form submit process

--- myexpress/index.js
+++ myexpress/index.js
@@ -2,6 +2,8 @@ var express = require("express")
 
 var app = express();
 
+
+
 var fortune = require('./lib/fortune.js');
 
 var handlebars = require('express3-handlebars')
@@ -41,6 +43,23 @@ app.use(function (req, res, next) {
     req.query.test === '1';
   next();
 })
+
+app.use(require('body-parser')());
+
+app.get('/newsletter', function (req, res) {
+  res.render('newsletter', {csrf: 'CSRF token goes here' });
+});
+
+
+app.post('/process', function(req, res){
+  console.log('Form (from querystring): ' + req.query.form);
+  console.log('CSRF token (from hidden form field): ' + req.body._csrf);
+  console.log('Name (from visible form field): ' + req.body.name);
+  console.log('Email (from visible form field): ' + req.body.email);
+  res.redirect(303, '/thank-you');
+});
+
+
 
 app.get('/', function (req, res) {
   res.render('home');
