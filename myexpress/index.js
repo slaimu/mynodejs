757e3d45dae43553fe4e1c7b79eddd84b2eab87f
Xiaowu Zhang <xiaowu.zhangv@gmail.com>
2016-09-15 02:29:01 +0800

    simple upload file

--- myexpress/index.js
+++ myexpress/index.js
@@ -2,7 +2,7 @@ var express = require("express")
 
 var app = express();
 
-
+var formidable = require('formidable');
 
 var fortune = require('./lib/fortune.js');
 
@@ -57,6 +57,26 @@ app.post('/process', function(req, res){
   console.log('Name (from visible form field): ' + req.body.name);
   console.log('Email (from visible form field): ' + req.body.email);
   res.redirect(303, '/thank-you');
+});
+
+
+
+app.get('/contest/vacation-photo',function(req,res){
+  var now = new Date();
+  res.render('contest/vacation-photo',{
+    year: now.getFullYear(),month: now.getMonth()
+  });
+});
+app.post('/contest/vacation-photo/:year/:month', function(req, res){
+  var form = new formidable.IncomingForm();
+  form.parse(req, function(err, fields, files){
+    if(err) return res.redirect(303, '/error');
+    console.log('received fields:');
+    console.log(fields);
+    console.log('received files:');
+    console.log(files);
+    res.redirect(303, '/thank-you');
+  });
 });
 
 
