06ddd6f4ece80eeb51a16e801170ddada10d77d3
Xiaowu Zhang <xiaowu.zhangv@gmail.com>
2016-09-15 01:36:41 +0800

    simple use case of handlebars in browser side

--- myexpress/index.js
+++ myexpress/index.js
@@ -68,6 +68,20 @@ app.get('/jquerytest', function (req, res) {
 });
 
 
+app.get('/nursery-rhyme', function (req, res) {
+  res.render('nursery-rhyme');
+});
+
+
+app.get('/data/nursery-rhyme', function (req, res) {
+  res.json({
+    animal: 'squirrel',
+    bodyPart:  'tail',
+    adjective: 'bushy',
+    noun: 'heck'
+  });
+});
+
 app.use(function (req, res) {
   res.status(404)
   res.render('404');
