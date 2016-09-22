217225b76f1a0fb4b643bb6e998f3cfbaf33a017
Xiaowu Zhang <xiaowu.zhangv@gmail.com>
2016-09-23 03:01:57 +0800

    a tiny command

---
+++ node_tutorial/tiny_command_line.js
@@ -0,0 +1,21 @@
+const readline = require('readline');
+const rl = readline.createInterface(process.stdin, process.stdout);
+
+rl.setPrompt('>>> ');
+rl.prompt();
+
+rl.on('line', (line) => {
+  switch(line.trim()) {
+    case 'hello':
+      console.log('world!');
+      break;
+    default:
+      console.log('Say what? I might have heard `' + line.trim() + '`');
+      break;
+  }
+  rl.prompt();
+});
+rl.on('close', () => {
+  console.log('Have a great day!');
+  process.exit(0);
+});
